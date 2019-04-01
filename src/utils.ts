f4ff975b052ffd24f0f6a71c4509698d340a4128
John Hardy <jh@lagado.com>
2019-04-01 23:21:51 +1100

    tweaks

--- src/utils.ts
+++ src/utils.ts
@@ -5,21 +5,3 @@ export function stringify(value: any) {
       (value as any).description :
       value.toString();
 }
-
-// function assert(text: string, expected: string, message: string, postProcess = stringify) {
-//   let result = parseEval(text, {...globalContext});
-//   if (result === undefined)
-//     throw 'Undefined';
-//   else {
-//     const actual = postProcess(result);
-//     if (actual === expected)
-//       console.log('Success!');
-//     else {
-//       throw `${message}
-//       ${text}
-//       expected:${expected} actual:${actual}
-//       `;
-//     }
-//   }
-// }
-
