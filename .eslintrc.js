a429884a11e9fd1f81ad75a3b8e798101daccc53
John Hardy <jh@lagado.com>
2019-05-02 23:03:27 +1000

    add stg

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,6 @@ module.exports = {
             },
         ],
         "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-member-accessibility": "off",
     },
 };
